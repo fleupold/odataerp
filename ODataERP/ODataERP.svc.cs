afea3f4d1bb2f317e6ab5bd3dfbbc54057fdb6ed
Georgi87 <stefan.george@genesisware.com>
2012-05-26 11:55:59 +0200

    Addes OData Service

---
+++ ODataERP/ODataERP.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace ODataERP
+{
+    public class ODataERP : DataService< ODataERPEntities >
+    {
+        // Diese Methode wird nur einmal aufgerufen, um dienstweite Richtlinien zu initialisieren.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: Regeln festlegen, die angeben, welche Entitätssets und welche Dienstvorgänge sichtbar, aktualisierbar usw. sind
+            // Beispiele:
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+    }
+}
